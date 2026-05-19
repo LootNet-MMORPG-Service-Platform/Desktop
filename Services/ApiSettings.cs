a10871bff568682a9b11942a59073555e2a05f4d
JanRojek <jan.rojek@studenci.collegiumwitelona.pl>
2026-05-19 16:45:30 +0200

    Switch API URL to deployed backend

--- Services/ApiSettings.cs
+++ Services/ApiSettings.cs
@@ -4,6 +4,6 @@ namespace desktop_app.Services;
 
 public static class ApiSettings
 {
-    //public static readonly Uri BaseUrl = new("https://lootnet-api.openrender.com");
-    public static readonly Uri BaseUrl = new("https://localhost:7124");
+    //public static readonly Uri BaseUrl = new("https://localhost:7124");
+    public static readonly Uri BaseUrl = new("https://lootnet-api.onrender.com");
 }
