c0c32d7ecddce5a9082ea0b07f580ef48836eedc
JanRojek <jan.rojek@studenci.collegiumwitelona.pl>
2026-05-28 16:22:13 +0200

    Configure API URL by build configuration

--- Services/ApiSettings.cs
+++ Services/ApiSettings.cs
@@ -4,6 +4,9 @@ namespace desktop_app.Services;
 
 public static class ApiSettings
 {
-    //public static readonly Uri BaseUrl = new("https://localhost:7124");
+#if DEBUG
+    public static readonly Uri BaseUrl = new("https://localhost:7124");
+#else
     public static readonly Uri BaseUrl = new("https://lootnet-api.onrender.com");
+#endif
 }
